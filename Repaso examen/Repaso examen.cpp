d6337101f79bab646cc6cc797641f4b9cd648ebe
CHRISTIAN ALEXANDER ELIZONDO MUÑOZ <celizondom@miumg.edu.gt>
2025-02-21 09:10:42 -0600

    Topo Gigio

--- Repaso examen/Repaso examen.cpp
+++ Repaso examen/Repaso examen.cpp
@@ -2,7 +2,68 @@
 
 using namespace std;
 
+class Estudiante {
+private:
+    string nombre;
+    int edad;
+public:
+    Estudiante(string nom, int ed) {
+        nombre = nom;
+        edad = ed;
+    }
+    string getNombre() {
+        return nombre;
+    }
+    int getEdad() {
+        return edad;
+    }
+};
+
+class EstudiantePrimaria : public Estudiante {
+private:
+    string grado;
+public:
+    EstudiantePrimaria(string nom, int ed, string gr) : Estudiante(nom, ed) {
+        grado = gr;
+    }
+    string getGrado() {
+        return grado;
+    }
+};
+
+class EstudianteSecundaria : public Estudiante {
+private:
+    string especialidad;
+public:
+    EstudianteSecundaria(string nom, int ed, string esp) : Estudiante(nom, ed) {
+        especialidad = esp;
+    }
+    string getEspecialidad() {
+        return especialidad;
+    }
+};
+
+class EstudianteUniversidad : public Estudiante {
+private:
+    string carrera;
+public:
+    EstudianteUniversidad(string nom, int ed, string car) : Estudiante(nom, ed) {
+        carrera = car;
+    }
+    string getCarrera() {
+        return carrera;
+    }
+};
+
 int main()
 {
-    cout << "Hello World!\n";
+    EstudiantePrimaria ep("Juan", 10, "5to Grado");
+    EstudianteSecundaria es("Maria", 15, "Ciencias");
+    EstudianteUniversidad eu("Pedro", 20, "Ingeniería");
+
+    cout << "Estudiante de Primaria: " << ep.getNombre() << ", Edad: " << ep.getEdad() << ", Grado: " << ep.getGrado() << endl;
+    cout << "Estudiante de Secundaria: " << es.getNombre() << ", Edad: " << es.getEdad() << ", Especialidad: " << es.getEspecialidad() << endl;
+    cout << "Estudiante de Universidad: " << eu.getNombre() << ", Edad: " << eu.getEdad() << ", Carrera: " << eu.getCarrera() << endl;
+
+    return 0;
 }
